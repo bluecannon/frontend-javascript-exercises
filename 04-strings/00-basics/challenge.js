6be8cf072276d7f949f87e2046e957c1e596097f
Harry Hsu <hhsu@mindspring.com>
2014-10-08 00:28:24 +0000

    string completed

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,34 @@
 module.exports.formLetter = function(firstName, senderName, message) {
 
+   var msg = firstName + "," + "\n\n" + message + "\n\n" + "Sincerely," + "\n" + senderName;
+   return ("Hello" + " " + msg);
 };
+
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
+   var strA = bigString.substring(startA, endA);
+   var strB = bigString.substring(startB, endB);
+   return (strA + strB);
+ };
 
-};
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  var idx = text.indexOf(searchString);
+  return(idx);
 };
+
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  var idx = text.lastIndexOf(searchString);
+  return(idx);
 };
 
-module.exports.substringBetweenMatches = function(text, searchString) {
 
+module.exports.substringBetweenMatches = function(text, searchString) {
+  var idx1 = text.indexOf(searchString);
+  var idx2 = text.lastIndexOf(searchString);
+
+  idx1 = idx1 + searchString.length;
+  var str = text.substring(idx1,idx2);
+  return(str);
 };
---
+++ 04-strings/00-basics/test.js
@@ -0,0 +1,8 @@
+
+var sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
+   return (bigString, startA, endA, startB, endB);
+};
+
+result = challenge.sliceItAndCombineIt(bigString, 10, 14, 25, 33);
+/*result = challenge.sliceItAndCombineIt(bigString, 10, 15, 27, 36);*/
+console.log(result);
