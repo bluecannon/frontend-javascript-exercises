4a29501aabc80bbb2f3f1ac4c22aeafb2e71c4b9
Harry Hsu <hhsu@mindspring.com>
2014-10-08 00:31:44 +0000

    arrays completed

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,19 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function (a,b,c,d) {
 
-module.exports.firstAndLast = undefined;
+  var newArray = [];
+  newArray.push(a);
+  newArray.push(b);
+  newArray.push(c);
+  newArray.push(d);
+
+  return newArray;
+};
+
+module.exports.firstAndLast = function(newArray) {
+
+  var myArray = [];
+  myArray.push(newArray [0]);
+  myArray.push(newArray [newArray.length-1]);
+  
+  return myArray;
+};
--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,35 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(textArray) {
+   var arrayLen = textArray.length;
+   var varRight;
+   var newArray = [];
 
-module.exports.plusesEverywhere = undefined;
+   while (arrayLen >= 1) {
+       varRight = textArray.pop();
+       newArray.push(varRight);
+       arrayLen --;
+   } 
+   newArray.unshift(1);
+   return (newArray);
+};
+
+module.exports.plusesEverywhere = function(textArray) {
+   var newStr = "";
+   var newArray = [];
+   var varRight = "";
+   var arrayLen = textArray.length;
+
+   while (arrayLen > 0)
+   {
+      varRight = textArray.shift();
+      newArray.push(varRight);
+      
+      if (arrayLen > 1)
+         newArray.push("+");     
+      arrayLen --;
+   }
+   
+   newStr = newArray.toString().replace(",","");
+   return (newStr.replace(/,/g, ''));
+};
 
 module.exports.arrayQuantityPlusOne = undefined;
